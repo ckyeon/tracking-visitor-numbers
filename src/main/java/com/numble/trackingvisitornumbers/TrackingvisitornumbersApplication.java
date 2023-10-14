e3929f99cdcc7fd92ab6e77cfb3c489a70b8482a
ckyeon <qsky8520@gmail.com>
2023-10-14 19:39:54 +0900

    initial commit

---
+++ src/main/java/com/numble/trackingvisitornumbers/TrackingvisitornumbersApplication.java
@@ -0,0 +1,13 @@
+package com.numble.trackingvisitornumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingvisitornumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingvisitornumbersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numble/trackingvisitornumbers/TrackingvisitornumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.trackingvisitornumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingvisitornumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
